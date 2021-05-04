b47dee2d9b4d8811d840d649deea7fe8ade2a05e
Manan Makwana <mananextra23@gmail.com>
2021-05-04 22:47:22 +0530

    resources added

--- Backend/breaks/shortBreak.js
+++ Backend/breaks/shortBreak.js
@@ -1,24 +1,102 @@
-const {ipcRenderer} = require("electron")
-
+const { ipcRenderer } = require("electron")
 const skipBtn = document.getElementById("skipBtn")
 skipBtn.disabled = false;
 
 var strict_flg = false;
 if (localStorage.getItem('strict')) {
     let strict_tmp = localStorage.getItem('strict');
-    if (strict_tmp==="true") {
+    if (strict_tmp === "true") {
         strict_flg = true;
-    }
-    else {
+    } else {
         strict_flg = false;
     }
 }
 
 // skipBtn.innerHTML = strict_flg;
 
-if (strict_flg===true) {
+if (strict_flg === true) {
     skipBtn.style.display = "none";
 }
+
+function randomidea() {
+    let defaultIdeas = ["Go grab a glass of water.",
+        "Slowly look all the way left, then right.",
+        "Slowly look all the way up, then down.",
+        "Close your eyes and take few deep breaths.",
+        "Stand from your chair and stretch.",
+        "Close your eyes and count your breaths.",
+        "Take a moment to smile at being alive."
+    ];
+    let IdeasArray = new Array();
+    IdeasArray = JSON.parse(localStorage.getItem('Ideas'));
+
+    if (IdeasArray === null) {
+        IdeasArray = defaultIdeas;
+    }
+    let index = Math.floor(Math.random() * IdeasArray.length);
+    return IdeasArray[index];
+}
+
+function MusicClicked() {
+    let musicStirng = localStorage.getItem('arr');
+    let musicArray = JSON.parse(musicStirng);
+
+    let temp = ['Audio.mp3'];
+    if (musicArray === null) {
+        musicArray = temp;
+    }
+
+    if (musicArray.length === 0) {
+        alert('No music files are present');
+        return;
+    }
+    let newMusic = document.createElement('audio');
+    console.log(musicArray.length);
+    // for (let i = 0; i < musicArray.length; i++) {
+    //     document.getElementById(musicArray[i]).pause();
+    //     document.getElementById(musicArray[i]).load();
+    // }
+    let ind = Math.floor(Math.random() * musicArray.length);
+
+    newMusic.controls = 'controls';
+    //temp.setAttribute('onclick', `fadeOutEffect(${i})`);
+    let temp1 = document.createElement('source');
+    temp1.src = musicArray[ind];
+    newMusic.appendChild(temp1);
+    newMusic.play();
+    console.log(newMusic);
+    fadeOutEffect();
+
+
+    function fadeOutEffect() {
+
+        // let audiosnippetId = musicArray[ind];
+        // var sound = document.getElementById(audiosnippetId);
+
+        // Set the point in playback that fadeout begins. This is for a 2 second fade out.
+        var fadePoint = 100;
+
+        var fadeAudio = setInterval(function() {
+            // console.log(sound.volume);
+            // Only fade if past the fade out point or not at zero already
+            if ((newMusic.currentTime >= fadePoint) && (newMusic.volume > 0.0)) {
+                newMusic.volume -= 0.1;
+            }
+            // When volume at zero stop all the intervalling
+            if (newMusic.volume <= 0.1) {
+                console.log("volume is zero");
+                sound.pause();
+                clearInterval(fadeAudio);
+            }
+        }, 200);
+
+    }
+
+}
+
+let msg = document.getElementById('Idea')
+let idea = randomidea();
+msg.innerHTML = idea;
 
 skipBtn.addEventListener('click', () => {
     skipBtn.disabled = true;
@@ -28,7 +106,7 @@ skipBtn.addEventListener('click', () => {
         let skip_tmp = localStorage.getItem('short_skipped');
         skip_num = parseInt(skip_tmp);
     }
-    skip_num = skip_num+1;
+    skip_num = skip_num + 1;
     localStorage.setItem('short_skipped', skip_num);
     ipcRenderer.send('Break-has-been-skipped')
 })
